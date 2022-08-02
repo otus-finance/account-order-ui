97e24bd7814e0a17952bcb9937d4cc1f3971ecc5
0xOtusBen <ben@otus.finance>
2022-08-02 15:41:23 -0500

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
@@ -16,13 +17,13 @@ const Home: NextPage = () => {
       <div className="relative z-50 w-full h-24">
           <div className="container flex items-center justify-center h-full max-w-6xl px-8 mx-auto sm:justify-between xl:px-0">
 
-              <a href="/" className="relative flex items-center inline-block h-5 h-full font-black leading-none">
+              <Link href="/" className="relative flex items-center inline-block h-5 h-full font-black leading-none">
                   <span className="ml-3 text-xl text-gray-800">tails<span className="text-indigo-600" data-primary="indigo-600">.</span></span>
-              </a>
+              </Link>
 
               <div className="absolute left-0 flex-col items-center justify-center hidden w-full pb-8 mt-48 border-b border-gray-200 md:relative md:w-auto md:bg-transparent md:border-none md:mt-0 md:flex-row md:p-0 md:items-end md:flex md:justify-between">
-                  <a href="#_" className="relative z-40 px-3 py-2 mr-0 text-sm font-bold text-indigo-600 md:px-5 sm:mr-3 md:mt-0" data-primary="indigo-600">Login</a>
-                  <a href="#_" className="relative z-40 inline-block w-auto h-full px-5 py-3 text-sm font-bold leading-none text-white transition-all transition duration-100 duration-300 bg-indigo-600 rounded shadow-md fold-bold sm:w-full lg:shadow-none hover:shadow-xl" data-primary="indigo-600" data-rounded="rounded">Get Started</a>
+                  <Link href="#_" className="relative z-40 px-3 py-2 mr-0 text-sm font-bold text-indigo-600 md:px-5 sm:mr-3 md:mt-0" data-primary="indigo-600">Login</a>
+                  <Link href="#_" className="relative z-40 inline-block w-auto h-full px-5 py-3 text-sm font-bold leading-none text-white transition-all transition duration-100 duration-300 bg-indigo-600 rounded shadow-md fold-bold sm:w-full lg:shadow-none hover:shadow-xl" data-primary="indigo-600" data-rounded="rounded">Get Started</a>
 
               </div>
 
