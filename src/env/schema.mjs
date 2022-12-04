44e9e2cf0cd6ec5cab47ec0de998b46bde8dd78d
Benjamin Efigenio <befigenio@gmail.com>
2022-12-04 22:02:50 +0200

    remove prisma and add mailerlite

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,7 +6,8 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  MAILER_LITE_API: z.string(),
+  MAILER_LITE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
