9583aa4f30ddc25646260c1c6ab5b728b9f192bc
Benjamin Efigenio <befigenio@gmail.com>
2023-02-11 14:14:06 +0200

    remove share build

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,2 @@
 export type { BuilderProviderState, BuilderAction } from './BuilderProvider';
 export { builderInitialState, builderReducer } from './BuilderProvider'
-
-export type { SharedBuildProviderState, SharedBuildAction } from './SharedBuildProvider';
-export { sharedBuildInitialState, sharedBuildReducer } from './SharedBuildProvider'
