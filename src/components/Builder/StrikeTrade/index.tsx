986c08dbee9c7ef8c05ad284fda6191967034175
Benjamin Efigenio <befigenio@gmail.com>
2023-03-04 16:56:43 +0200

    check for market to display trade

--- src/components/Builder/StrikeTrade/index.tsx
+++ src/components/Builder/StrikeTrade/index.tsx
@@ -33,7 +33,7 @@ export const StrikeTrade = () => {
     strikes,
     positionPnl
   } = useBuilderContext();
-  console.log({ selectedMarket })
+
   const {
     netCreditDebit,
     collateralRequired,
@@ -127,185 +127,190 @@ export const StrikeTrade = () => {
 
   const { handleUpdateQuote } = useBuilderContext();
 
-  return <div className='col-span-2 sm:col-span-2 grid grid-cols-2'>
+  return <>
+    {
+      selectedMarket &&
+      <div className='col-span-1 sm:col-span-1 grid grid-cols-1'>
 
-    <div className="col-span-2">
+        <div className="col-span-1">
 
-      {
-        strikes.map((strike, index) => {
-          const { quote: { size, pricePerOption, isCall, isBuy }, strikePrice } = strike;
-          return <div key={index} className="border-b border-zinc-800">
-            <div className='p-2'>
-              <div className='text-white font-semibold text-md p-2'>
-                {`${isBuy ? 'Buy' : 'Sell'} ${formatName(selectedMarket?.name)} ${formatUSD(strikePrice, { dps: 2 })}  ${isCall ? 'Call' : 'Put'} `}
+          {
+            strikes.map((strike, index) => {
+              const { quote: { size, pricePerOption, isCall, isBuy }, strikePrice } = strike;
+              return <div key={index} className="border-b border-zinc-800">
+                <div className='p-2'>
+                  <div className='text-white font-semibold text-sm p-2'>
+                    {`${isBuy ? 'Buy' : 'Sell'} ${formatName(selectedMarket?.name)} ${formatUSD(strikePrice, { dps: 2 })}  ${isCall ? 'Call' : 'Put'} `}
+                  </div>
+                  <div className="flex items-center justify-between p-2">
+                    <p className="truncate font-sans text-xs font-normal text-white">
+                      Contracts
+                    </p>
+                    <div className="ml-2 flex flex-shrink-0">
+                      <label htmlFor="size" className="sr-only">
+                        Size
+                      </label>
+                      <div className="mt-1">
+                        <DebounceInput
+                          minLength={1}
+                          debounceTimeout={300}
+                          onChange={async (e) => {
+                            if (e.target.value == '') return
+                            const value = parseFloat(e.target.value);
+                            handleUpdateQuote({ size: value.toString(), strike: strike });
+                          }}
+                          type="number"
+                          name="size"
+                          id="size"
+                          value={fromBigNumber(size)}
+                          className="block w-24 rounded-sm border border-zinc-700 bg-transparent px-4 pr-2 py-2 text-right text-zinc-200 shadow-sm  sm:text-xs"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between p-2">
+                    <p className="truncate font-sans text-xs font-normal text-white">
+                      Price Per Option
+                    </p>
+                    <div className="ml-2 flex flex-shrink-0">
+                      <p className="inline-flex font-mono text-xs font-normal leading-5 text-white">
+                        {formatUSD(fromBigNumber(pricePerOption))}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
               </div>
+            })
+          }
+        </div>
+
+        <div className="col-span-1">
+          <div className='p-2'>
+            <div className='flex gap-8'>
+              <div className='p-2 text-xs text-zinc-200 hover:font-semibold cursor-pointer'>Market</div>
+              <div className='p-2 text-xs text-zinc-200 hover:font-semibold cursor-pointer'>Limit</div>
+              <div className='p-2 text-xs text-zinc-200 hover:font-semibold cursor-pointer'>Trigger</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 border-b border-zinc-800">
+          <div className='p-4'>
+
+            <div className='border border-zinc-800 p-2'>
               <div className="flex items-center justify-between p-2">
-                <p className="truncate font-sans text-xs font-normal text-white">
-                  Contracts
-                </p>
+                <div>
+                  <p className="truncate font-sans text-xs font-normal text-white">
+                    Price Per Option
+                  </p>
+                  <div>
+                    test
+                  </div>
+                </div>
+
                 <div className="ml-2 flex flex-shrink-0">
-                  <label htmlFor="size" className="sr-only">
-                    Size
-                  </label>
-                  <div className="mt-1">
-                    <DebounceInput
-                      minLength={1}
-                      debounceTimeout={300}
-                      onChange={async (e) => {
-                        if (e.target.value == '') return
-                        const value = parseFloat(e.target.value);
-                        handleUpdateQuote({ size: value.toString(), strike: strike });
-                      }}
-                      type="number"
-                      name="size"
-                      id="size"
-                      value={fromBigNumber(size)}
-                      className="block w-24 rounded-sm border border-zinc-700 bg-transparent px-4 pr-2 py-2 text-right text-zinc-200 shadow-sm  sm:text-xs"
-                    />
+                  <p className="inline-flex font-mono text-xs font-normal leading-5 text-white">
+                    Current Price: $10.22
+                  </p>
+                  <div>
+                    USD
                   </div>
                 </div>
               </div>
-              <div className="flex items-center justify-between p-2">
-                <p className="truncate font-sans text-xs font-normal text-white">
-                  Price Per Option
-                </p>
-                <div className="ml-2 flex flex-shrink-0">
-                  <p className="inline-flex font-mono text-xs font-normal leading-5 text-white">
-                    {formatUSD(fromBigNumber(pricePerOption))}
-                  </p>
-                </div>
-              </div>
             </div>
 
           </div>
-        })
-      }
-    </div>
-
-    <div className="col-span-2 ">
-      <div className='p-2'>
-        <div className='flex gap-8'>
-          <div className='p-2 text-xs text-zinc-200 hover:font-semibold cursor-pointer'>Market</div>
-          <div className='p-2 text-xs text-zinc-200 hover:font-semibold cursor-pointer'>Limit</div>
-          <div className='p-2 text-xs text-zinc-200 hover:font-semibold cursor-pointer'>Trigger</div>
         </div>
-      </div>
-    </div>
 
-    <div className="col-span-2 border-b border-zinc-800">
-      <div className='p-4'>
+        <div className='col-span-1 grid grid-cols-2'>
 
-        <div className='border border-zinc-800 p-2'>
-          <div className="flex items-center justify-between p-2">
-            <div>
-              <p className="truncate font-sans text-xs font-normal text-white">
-                Price Per Option
-              </p>
-              <div>
-                test
-              </div>
-            </div>
-
-            <div className="ml-2 flex flex-shrink-0">
-              <p className="inline-flex font-mono text-xs font-normal leading-5 text-white">
-                Current Price: $10.22
-              </p>
-              <div>
-                USD
-              </div>
-            </div>
+          <div className='text-xs text-zinc-200'>
+            Min. Premium Received
           </div>
+          <div className='text-xs text-white font-semibold col-span-1 text-right'>{formatUSD(netCreditDebit < 0 ? 0 : netCreditDebit)}</div>
+
+          <div className=' col-span-1 text-xs text-zinc-200'>
+            Collateral Required
+          </div>
+          <div className='text-xs text-white font-semibold col-span-1 text-right'>{formatUSD(collateralRequired)}</div>
+
+          <div className=' col-span-1 text-xs text-zinc-200'>
+            Max Cost
+          </div>
+          <div className='text-xs text-white font-semibold col-span-1 text-right'>{formatUSD(maxCost)}</div>
+
+          <div className='text-xs text-zinc-200 border-t border-zinc-700'>
+            Total Funds Required
+          </div>
+          <div className='text-xs text-white font-semibold col-span-1 text-right border-t border-zinc-700'>{formatUSD(collateralRequired + maxCost)}</div>
+
+
+          <div className='text-xs text-zinc-200 border-t border-zinc-700'>
+            {quoteAsset?.symbol} Balance
+          </div>
+          <div className='text-xs text-white font-semibold col-span-1 text-right border-t border-zinc-700'>{quoteAsset && formatUSD(fromBigNumber(quoteAsset.balance))}</div>
+
+        </div>
+
+        <div className="col-span-1 p-4">
+
+          {/* is loading */}
+          {
+            isLoading &&
+            <div onClick={() => console.warn('Add funds')} className="cursor-disabled border border-zinc-800 hover:border-emerald-800 bg-zinc-800 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
+              <Spinner />
+            </div>
+          }
+
+          {/* insufficient balance */}
+          {
+            isConnected && chain?.id === selectedChain?.chainId && quoteAsset && quoteAsset.balance.isZero() &&
+            <div onClick={() => console.warn('Add funds')} className="cursor-disabled border border-zinc-800 hover:border-emerald-800 bg-zinc-800 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
+              Insufficient Balance
+            </div>
+          }
+
+
+          {/* wallet connected / correct chain / quote asset not approved */}
+          {
+            isConnected && chain?.id === selectedChain?.chainId && quoteAsset && quoteAsset.tradeAllowance.isZero() && !quoteAsset.balance.isZero() &&
+            <div onClick={() => handleApproveQuote()} className="cursor-pointer border border-zinc-800 hover:bg-emerald-600 bg-zinc-900 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
+              Approve Quote
+            </div>
+          }
+
+          {/* wallet connected / correct chain / quote asset approved */}
+          {
+            isConnected && chain?.id === selectedChain?.chainId && quoteAsset && !quoteAsset.tradeAllowance.isZero() && !quoteAsset.balance.isZero() &&
+            <div onClick={() => handleExecuteMultiTrade()} className="cursor-pointer border border-emerald-700 hover:bg-emerald-600 bg-zinc-900 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
+              {isLoadingTx ? <Spinner /> : 'Execute Trade'}
+            </div>
+          }
+
+          {/* wallet connected but wrong chain */}
+          {
+            isConnected && chain?.id != selectedChain?.chainId &&
+
+            <div onClick={openChainModal} className="cursor-pointer border border-zinc-700 hover:border-emerald-700 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
+              Wrong network
+            </div>
+          }
+
+          {/* wallet not connected */}
+          {
+            !isLoading && !isConnected && openConnectModal &&
+            <div onClick={openConnectModal} className="cursor-pointer border border-zinc-700 hover:border-emerald-700 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
+              Connect Wallet
+            </div>
+          }
+
+
         </div>
 
       </div>
-    </div>
-
-    <div className='col-span-2 grid grid-cols-2'>
-
-      <div className='text-xs text-zinc-200'>
-        Min. Premium Received
-      </div>
-      <div className='text-xs text-white font-semibold col-span-1 text-right'>{formatUSD(netCreditDebit < 0 ? 0 : netCreditDebit)}</div>
-
-      <div className=' col-span-1 text-xs text-zinc-200'>
-        Collateral Required
-      </div>
-      <div className='text-xs text-white font-semibold col-span-1 text-right'>{formatUSD(collateralRequired)}</div>
-
-      <div className=' col-span-1 text-xs text-zinc-200'>
-        Max Cost
-      </div>
-      <div className='text-xs text-white font-semibold col-span-1 text-right'>{formatUSD(maxCost)}</div>
-
-      <div className='text-xs text-zinc-200 border-t border-zinc-700'>
-        Total Funds Required
-      </div>
-      <div className='text-xs text-white font-semibold col-span-1 text-right border-t border-zinc-700'>{formatUSD(collateralRequired + maxCost)}</div>
-
-
-      <div className='text-xs text-zinc-200 border-t border-zinc-700'>
-        {quoteAsset?.symbol} Balance
-      </div>
-      <div className='text-xs text-white font-semibold col-span-1 text-right border-t border-zinc-700'>{quoteAsset && formatUSD(fromBigNumber(quoteAsset.balance))}</div>
-
-    </div>
-
-    <div className="col-span-2 p-4">
-
-      {/* is loading */}
-      {
-        isLoading &&
-        <div onClick={() => console.warn('Add funds')} className="cursor-disabled border border-zinc-800 hover:border-emerald-800 bg-zinc-800 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
-          <Spinner />
-        </div>
-      }
-
-      {/* insufficient balance */}
-      {
-        isConnected && chain?.id === selectedChain?.chainId && quoteAsset && quoteAsset.balance.isZero() &&
-        <div onClick={() => console.warn('Add funds')} className="cursor-disabled border border-zinc-800 hover:border-emerald-800 bg-zinc-800 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
-          Insufficient Balance
-        </div>
-      }
-
-
-      {/* wallet connected / correct chain / quote asset not approved */}
-      {
-        isConnected && chain?.id === selectedChain?.chainId && quoteAsset && quoteAsset.tradeAllowance.isZero() && !quoteAsset.balance.isZero() &&
-        <div onClick={() => handleApproveQuote()} className="cursor-pointer border border-zinc-800 hover:bg-emerald-600 bg-zinc-900 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
-          Approve Quote
-        </div>
-      }
-
-      {/* wallet connected / correct chain / quote asset approved */}
-      {
-        isConnected && chain?.id === selectedChain?.chainId && quoteAsset && !quoteAsset.tradeAllowance.isZero() && !quoteAsset.balance.isZero() &&
-        <div onClick={() => handleExecuteMultiTrade()} className="cursor-pointer border border-emerald-700 hover:bg-emerald-600 bg-zinc-900 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
-          {isLoadingTx ? <Spinner /> : 'Execute Trade'}
-        </div>
-      }
-
-      {/* wallet connected but wrong chain */}
-      {
-        isConnected && chain?.id != selectedChain?.chainId &&
-
-        <div onClick={openChainModal} className="cursor-pointer border border-zinc-700 hover:border-emerald-700 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
-          Wrong network
-        </div>
-      }
-
-      {/* wallet not connected */}
-      {
-        !isLoading && !isConnected && openConnectModal &&
-        <div onClick={openConnectModal} className="cursor-pointer border border-zinc-700 hover:border-emerald-700 p-2 col-span-3 font-normal text-sm text-white text-center rounded-2xl">
-          Connect Wallet
-        </div>
-      }
-
-
-    </div>
-
-  </div>
+    }
+  </>
 }
 
 const isCreditOrDebit = (isBuy: boolean, usd: string): string => {
