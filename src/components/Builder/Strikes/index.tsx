0478020d0d6787aa72dab23392e6fc16430e48aa
Benjamin Efigenio <befigenio@gmail.com>
2023-03-06 13:13:07 +0200

    option to build strikes without strategy

--- src/components/Builder/Strikes/index.tsx
+++ src/components/Builder/Strikes/index.tsx
@@ -12,24 +12,34 @@ import Modal from '../../UI/Modal';
 import { StrikeTrade } from '../StrikeTrade';
 import ArbitrumIcon from '../../UI/Icons/Color/ONE';
 import OptimismIcon from '../../UI/Icons/Color/OP';
-import { AccountContextProvider } from '../../../context/AccountContext';
+import { AccountContextProvider } from '../../../context/LyraAccountContext';
+import { BuilderType } from '../../../utils/types';
 
 export const Strikes = () => {
 
   const {
+    builderType,
     isBuildingNewStrategy,
     showStrikesSelect,
     selectedStrategy,
     handleBuildNewStrategy
   } = useBuilderContext();
 
-  return <div className='col-span-3 sm:col-span-3 p-6 grid grid-cols-6'>
+  return <div className='col-span-3 sm:col-span-3 px-6 pb-6 grid grid-cols-6'>
+
+    {
+      builderType === BuilderType.Custom &&
+      <div className='col-span-6'>
+        <SelectStrikesTable />
+      </div>
+
+    }
 
     {/* custom strategy button */}
     <div className="sm:col-end-7 sm:col-span-2 col-start-1 col-end-7">
       <div className="col-span-1 grid grid-cols-3 gap-3 mt-2">
         {
-          selectedStrategy &&
+          selectedStrategy && builderType === BuilderType.Builder &&
           <div onClick={() => handleBuildNewStrategy(!isBuildingNewStrategy)} className="cursor-pointer border border-zinc-800 hover:border-emerald-700 bg-zinc-900 p-2 col-span-3 font-semibold text-xs text-white text-center rounded-2xl">
             {isBuildingNewStrategy ? 'Reset Strategy' : 'Use Strategy as Template'}
           </div>
@@ -38,21 +48,20 @@ export const Strikes = () => {
     </div>
 
     {
-      showStrikesSelect &&
+      showStrikesSelect && builderType === BuilderType.Builder &&
       <motion.div className='col-span-6' animate={showStrikesSelect ? "open" : "closed"} variants={{
         open: { opacity: 1, x: 0 },
         closed: { opacity: 0, x: "-100%" },
       }}>
         <SelectStrikesTable />
       </motion.div>
-
     }
 
     <div className='col-span-6 overflow-x-scroll scrollbar scrollbar-thumb-zinc-700 scrollbar-track-zinc-500 rounded-sm'>
       <div className="flex items-center pt-2 pb-2">
-        <LyraIcon />
+        <LyraIcon className='h-4 w-4' />
         <div className='pl-2 font-light text-xs uppercase'>
-          <strong> Powered by lyra.finance </strong>
+          <strong>Powered by lyra.finance </strong>
         </div>
       </div>
       <StrikesTable />
--- src/components/Builder/index.tsx
+++ src/components/Builder/index.tsx
@@ -5,20 +5,23 @@ import { Market } from './Market'
 import { Strategy } from './Strategy'
 import { Strikes } from './Strikes'
 
-import { AccountContextProvider } from '../../context/AccountContext';
+import { LyraAccountContextProvider } from '../../context/LyraAccountContext';
 import { useBuilderContext } from '../../context/BuilderContext'
 import { StrikeTrade } from './StrikeTrade'
+import { BuilderType } from '../../utils/types'
+import { ArrowLeftCircleIcon } from '@heroicons/react/20/solid'
 
 export const OptionsBuilder = () => {
 
   const {
     lyra,
-    strikes
+    strikes,
+    builderType,
   } = useBuilderContext();
 
   return <div className="grid sm:grid-cols-3 grid-cols-3 gap-8">
 
-    <div className='col-span-2 '>
+    <div className='col-span-2'>
 
       <div className='border border-zinc-800 rounded-sm shadow-sm'>
         <div className='p-6'>
@@ -26,30 +29,34 @@ export const OptionsBuilder = () => {
         </div>
 
         <div className='border-b border-zinc-800'>
-          <div className='p-6'>
-            <BuilderType />
+          <div className='px-6 pt-2 pb-4'>
+            <BuilderSelect />
           </div>
         </div>
 
-        <div className='border-b border-zinc-800'>
-          <Strategy />
-        </div>
+        {
+          builderType === BuilderType.Builder &&
+          <div className='border-b border-zinc-800'>
 
-        <div className='border-b border-zinc-800'>
+            <Strategy />
+
+          </div>
+        }
+
+        <div>
           <Strikes />
         </div>
       </div>
 
       <div>
 
-        <AccountInfo />
+        <AccountInfoSelect />
 
         <div className='border border-zinc-800 rounded-sm p-6'>
-          test
+          <AccountInfo />
         </div>
 
       </div>
-
 
     </div>
 
@@ -58,15 +65,29 @@ export const OptionsBuilder = () => {
     <div className='col-span-1'>
       <div className=' border border-zinc-800 rounded-sm shadow-sm'>
         {
-          lyra && strikes[0] &&
-          <AccountContextProvider lyra={lyra} strike={strikes[0]}>
-            <StrikeTrade />
-          </AccountContextProvider>
-        }
+          lyra && strikes[0] ? <>
+            <LyraAccountContextProvider lyra={lyra} strike={strikes[0]}>
+              <StrikeTrade />
+            </LyraAccountContextProvider>
 
-        <div className='p-4 rounded-sm'>
-          <Chart />
-        </div>
+            <div className='p-4 rounded-sm'>
+              <Chart />
+            </div>
+          </> :
+            <div className='bg-black p-4'>
+              <div className="flex items-center p-2">
+                <p className="truncate font-sans text-xs font-normal text-white">
+                  <ArrowLeftCircleIcon className="h-5 w-5 text-white"
+                    aria-hidden="true" />
+                </p>
+                <div className="ml-2 flex flex-shrink-0">
+                  <p className="inline-flex font-mono text-sm font-normal leading-5 text-white">
+                    Select Strikes
+                  </p>
+                </div>
+              </div>
+            </div>
+        }
       </div>
     </div>
     {/* 
@@ -84,27 +105,34 @@ export const OptionsBuilder = () => {
 
 }
 
-const BuilderType = () => {
+const BuilderSelect = () => {
+
+  const { builderType, handleSelectBuilderType } = useBuilderContext();
+
   return <div className='flex items-center gap-4 text-sm'>
-    <div>
+    <div onClick={() => handleSelectBuilderType(BuilderType.Builder)} className={`cursor-pointer hover:text-white ${builderType === BuilderType.Builder ? 'text-white underline' : 'text-zinc-300'}`}>
       Builder
     </div>
-    <div>
+    <div onClick={() => handleSelectBuilderType(BuilderType.Custom)} className={`cursor-pointer hover:text-white ${builderType === BuilderType.Custom ? 'text-white underline' : 'text-zinc-300'}`}>
       Custom
     </div>
   </div>
 }
 
-const AccountInfo = () => {
+const AccountInfoSelect = () => {
   return <div className='flex items-center gap-8 text-sm px-6 pt-6 pb-2'>
-    <div>
+    <div className='cursor-pointer text-zinc-300 hover:text-white'>
       Positions
     </div>
-    <div>
+    <div className='cursor-pointer text-zinc-300 hover:text-white'>
       Orders
     </div>
-    <div>
+    <div className='cursor-pointer text-zinc-300 hover:text-white'>
       Trades
     </div>
   </div>
+}
+
+const AccountInfo = () => {
+  return <>test</>
 }
