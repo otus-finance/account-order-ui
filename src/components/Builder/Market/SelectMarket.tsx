41b3b49895943e312a074733b9c08c1e84963e94
Benjamin Efigenio <befigenio@gmail.com>
2023-03-24 13:10:22 +0200

    style update on bg

--- src/components/Builder/Market/SelectMarket.tsx
+++ src/components/Builder/Market/SelectMarket.tsx
@@ -1,50 +1,59 @@
-import React, { Dispatch } from 'react'
-import BTCIcon from '../../UI/Icons/Color/BTC'
-import ETHIcon from '../../UI/Icons/Color/ETH'
-import { LyraBoard, LyraMarket } from '../../../queries/lyra/useLyra'
-import { Spinner } from '../../UI/Components/Spinner'
+import React, { Dispatch } from "react";
+import BTCIcon from "../../UI/Icons/Color/BTC";
+import ETHIcon from "../../UI/Icons/Color/ETH";
+import { LyraBoard, LyraMarket } from "../../../queries/lyra/useLyra";
+import { Spinner } from "../../UI/Components/Spinner";
 
 export const formatName = (marketName: string) => {
-  switch (marketName) {
-    case 'sETH-sUSD':
-    case 'ETH-USDC':
-      return 'ETH';
-    default:
-      return 'BTC'
-  }
-}
+	switch (marketName) {
+		case "sETH-sUSD":
+		case "ETH-USDC":
+			return "ETH";
+		default:
+			return "BTC";
+	}
+};
 
-export const LyraMarketOptions = (
-  { markets, selectedMarket, handleSelectedMarket }:
-    { markets: LyraMarket[] | null, selectedMarket: LyraMarket | null, handleSelectedMarket?: any }
-) => {
+export const LyraMarketOptions = ({
+	markets,
+	selectedMarket,
+	handleSelectedMarket,
+}: {
+	markets: LyraMarket[] | null;
+	selectedMarket: LyraMarket | null;
+	handleSelectedMarket?: any;
+}) => {
+	return (
+		markets && (
+			<div className="flex">
+				{markets
+					.filter(({ liveBoards }: { liveBoards: LyraBoard[] }) => liveBoards.length > 0)
+					.map((market: LyraMarket, index: number) => {
+						const { name } = market;
+						const isSelected = selectedMarket?.name == name;
+						const selectedClass = isSelected ? "bg-black border-none" : "border-none";
+						return (
+							<div
+								key={index}
+								onClick={() => {
+									handleSelectedMarket && handleSelectedMarket(market);
+								}}
+								className={`p-2 px-4 border hover:border-emerald-600 rounded-sm first:mr-1  cursor-pointer ${selectedClass}`}
+							>
+								<div className="flex items-center">
+									{name == "sETH-sUSD" && <ETHIcon className="h-8 w-8" />}
+									{name == "ETH-USDC" && <ETHIcon className="h-8 w-8" />}
 
-  return markets && <div className='flex'>
-    {
-      markets
-        .filter(({ liveBoards }: { liveBoards: LyraBoard[] }) => liveBoards.length > 0)
-        .map((market: LyraMarket, index: number) => {
-          const { name } = market;
-          const isSelected = selectedMarket?.name == name;
-          const selectedClass = isSelected ? 'bg-zinc-900 border-none' : 'border-none'
-          return <div
-            key={index}
-            onClick={() => {
-              handleSelectedMarket && handleSelectedMarket(market);
-            }}
-            className={`p-2 px-3 border hover:border-emerald-600 hover:bg-zinc-900 rounded-sm first:mr-1  cursor-pointer ${selectedClass}`}>
-            <div className="flex items-center">
-              {name == 'sETH-sUSD' && <ETHIcon className="h-8 w-8" />}
-              {name == 'ETH-USDC' && <ETHIcon className="h-8 w-8" />}
-
-              {name == 'sBTC-sUSD' && <BTCIcon className="h-8 w-8" />}
-              {name == 'WBTC-USDC' && <BTCIcon className="h-8 w-8" />}
-              <div className="pl-2">
-                <strong className='text-md'>{formatName(name)}</strong>
-              </div>
-            </div>
-          </div>
-        })
-    }
-  </div>
-}
+									{name == "sBTC-sUSD" && <BTCIcon className="h-8 w-8" />}
+									{name == "WBTC-USDC" && <BTCIcon className="h-8 w-8" />}
+									<div className="pl-1 pr-2">
+										<strong className="text-md">{formatName(name)}</strong>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+			</div>
+		)
+	);
+};
