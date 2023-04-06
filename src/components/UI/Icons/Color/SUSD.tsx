6a098015d90dd464188f586dd1d9b994726d3164
Benjamin Efigenio <befigenio@gmail.com>
2023-04-06 19:51:39 +0300

    increase size to use on ranged market token cards

--- src/components/UI/Icons/Color/SUSD.tsx
+++ src/components/UI/Icons/Color/SUSD.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 
 const SUSDIcon = (props: any) => (
 	<svg
-		width={40}
-		height={40}
+		width={44}
+		height={44}
 		viewBox="0 0 30 30"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
@@ -18,8 +18,8 @@ const SUSDIcon = (props: any) => (
 				maskUnits="userSpaceOnUse"
 				x={0}
 				y={0}
-				width={40}
-				height={40}
+				width={44}
+				height={44}
 			>
 				<path
 					d="M15 30C23.2843 30 30 23.2843 30 15C30 6.71573 23.2843 0 15 0C6.71573 0 0 6.71573 0 15C0 23.2843 6.71573 30 15 30Z"
