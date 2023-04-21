e500d147bba5affa44d244a8879a64ea7733cbad
Benjamin Efigenio <befigenio@gmail.com>
2023-04-21 20:11:18 +0300

    add activity type tab

--- src/components/Builder/index.tsx
+++ src/components/Builder/index.tsx
@@ -7,14 +7,14 @@ import { Strikes } from "./Strikes";
 import { LyraAccountContextProvider } from "../../context/LyraAccountContext";
 import { useBuilderContext } from "../../context/BuilderContext";
 import { StrikeTrade } from "./StrikeTrade";
-import { BuilderType } from "../../utils/types";
+import { ActivityType, BuilderType } from "../../utils/types";
 import { ArrowLeftCircleIcon } from "@heroicons/react/20/solid";
 import { AccountPosition } from "../Account";
 
 import { Spinner } from "../UI/Components/Spinner";
 
 export const OptionsBuilder = () => {
-	const { lyra, strikes, builderType, isValid } = useBuilderContext();
+	const { lyra, strikes, builderType, activityType } = useBuilderContext();
 
 	return (
 		<div className="grid sm:grid-cols-3 grid-cols-3 gap-8">
@@ -41,50 +41,70 @@ export const OptionsBuilder = () => {
 					</div>
 				</div>
 
-				<div className="hidden sm:block">
-					<AccountPosition />
-				</div>
+				<div className="hidden sm:block">{/* <AccountPosition /> */}</div>
 			</div>
 
 			<div className="col-span-3 sm:col-span-1">
 				<div className="border border-zinc-800 rounded-lg shadow-md shadow-black">
-					{lyra && strikes[0] ? (
-						<>
-							<LyraAccountContextProvider lyra={lyra} strike={strikes[0]}>
-								<StrikeTrade />
-							</LyraAccountContextProvider>
+					<ActivitySelect />
+					{activityType === ActivityType.Trade ? (
+						<div>
+							{lyra && strikes[0] ? (
+								<>
+									<LyraAccountContextProvider lyra={lyra}>
+										<StrikeTrade />
+									</LyraAccountContextProvider>
 
-							<div className="p-4 border-t border-zinc-800">
-								<Chart />
-							</div>
-						</>
-					) : (
-						<div className=" p-4">
-							<div className="flex items-center p-2">
-								<p className="truncate font-sans text-xs font-normal text-white">
-									<ArrowLeftCircleIcon className="h-5 w-5 text-white" aria-hidden="true" />
-								</p>
-								<div className="ml-2 flex flex-shrink-0">
-									<p className="inline-flex font-mono text-sm font-normal leading-5 text-white">
-										Select Strikes
-									</p>
+									<div className="p-4 border-t border-zinc-800">
+										<Chart />
+									</div>
+								</>
+							) : (
+								<div className=" p-4">
+									<div className="flex items-center p-2">
+										<p className="truncate font-sans text-xs font-normal text-white">
+											<ArrowLeftCircleIcon className="h-5 w-5 text-white" aria-hidden="true" />
+										</p>
+										<div className="ml-2 flex flex-shrink-0">
+											<p className="inline-flex font-mono text-sm font-normal leading-5 text-white">
+												Select Strikes
+											</p>
+										</div>
+									</div>
 								</div>
-							</div>
-
-							<div className="flex items-center p-2">
-								{!isValid && (
-									<p className="font-mono text-sm font-normal leading-5 text-white">
-										Strikes for strategy not available for selected assets or expiry.
-									</p>
-								)}
-							</div>
+							)}
 						</div>
+					) : (
+						<div>positions</div>
 					)}
 				</div>
 			</div>
 
 			<div className="col-span-3 sm:hidden">
 				<AccountPosition />
+			</div>
+		</div>
+	);
+};
+
+const ActivitySelect = () => {
+	const { activityType, handleSelectActivityType } = useBuilderContext();
+
+	return (
+		<div className="flex justify-between border-b border-zinc-800">
+			<div
+				onClick={() => handleSelectActivityType(ActivityType.Trade)}
+				className={`cursor-pointer p-4 w-full text-center text-sm font-mono border-r border-zinc-800 
+		${activityType === ActivityType.Trade ? "text-white font-semibold" : "text-zinc-300"}`}
+			>
+				Trade
+			</div>
+			<div
+				onClick={() => handleSelectActivityType(ActivityType.Position)}
+				className={`cursor-pointer p-4 w-full text-center text-sm font-mono border-r border-zinc-800 
+		${activityType === ActivityType.Position ? "text-white font-semibold" : "text-zinc-300"}`}
+			>
+				Active Positions
 			</div>
 		</div>
 	);
