9af71c4349af7f8d465ca9f0feef12ebb3f14f0a
Benjamin Efigenio <befigenio@gmail.com>
2023-04-27 11:59:37 +0300

    fix on otusfee

--- src/constants/markets.ts
+++ src/constants/markets.ts
@@ -3,6 +3,6 @@ import { ethers } from "ethers";
 export const ETH_MARKET = ethers.utils.formatBytes32String("ETH");
 export const BTC_MARKET = ethers.utils.formatBytes32String("BTC");
 
-export const OTUS_FEE = 0.3; // 30% fee / YEAR
+export const OTUS_FEE = 0.2; // 50% fee / YEAR
 export const MAX_NUMBER = Infinity; // Max positive number
 export const MIN_NUMBER = 0; // Max positive number
